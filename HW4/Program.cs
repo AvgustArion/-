c2d1216fd705f41e5e947854200fbe5e0f8ef6c1
Avgust <dr.impu1s@yandex.ru>
2023-02-16 17:48:18 +0500

    Fourth

---
+++ HW4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int zero = 0;
+while (zero < n - 1)
+{
+    zero = zero + 2;
+    Console.WriteLine(zero);
+}
