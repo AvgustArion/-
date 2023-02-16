98427d9ca3df8395cc69b76ef87fc4350c5296d3
Avgust <dr.impu1s@yandex.ru>
2023-02-16 16:19:20 +0500

    First

---
+++ HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+a = max;
+if (a > b)
+{
+    max = a;
+    Console.WriteLine($"Наибольшее число {max}");
+}
+else 
+{
+    max = b;
+    Console.WriteLine($"Наибольшее число {max}");
+}
